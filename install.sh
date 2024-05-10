93716facf730035902e88d7e428a4c0c2471a936
Тимур Байдюсенов <63646703+baitim@users.noreply.github.com>
2024-05-10 21:03:44 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-python3 -m venv venv
+wget https://bootstrap.pypa.io/virtualenv/3.11/virtualenv.pyz
+python3 virtualenv.pyz venv
 source venv/bin/activate
 pip install -r requirements.txt
